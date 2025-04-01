dde69f9aa2364346aa483b3c822b55f736743533
Cristian <csobrinom@alumnos.nebrija.es>
2025-04-01 10:39:39 +0200

    Limpieza del main y implementacion de los metodos en la clase jugar

--- quienEsQuienPrueba1/src/controller/Main.java
+++ quienEsQuienPrueba1/src/controller/Main.java
@@ -6,13 +6,13 @@ import java.util.Scanner;
 
 import model.*;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		// VARIABLES//
-		boolean salirMenu = false;
-		boolean salirJuego = false;
+
+
 		// ARRAYLIST PERSONAJES //
 		ArrayList<Personaje> listaPersonajes = new ArrayList<Personaje>();
 		listaPersonajes.add(new Personaje("Carlos", TipoPelo.CORTO, ColorPelo.CASTANYO, Ojos.MARRONES, Genero.HOMBRE,
@@ -38,80 +38,15 @@ public class Main {
 
 		Personaje aleatorio = Personaje.obtenerPersonajeAleatorio(listaPersonajes);
 	    // aleatorio.imprimirPersonaje(); //
+		
+		Juego juego = new Juego();
+        juego.menuGeneral(listaPersonajes);
+        juego.jugar();
  
-		// BUCLE PRINCIPAL DEL MENÚ //
-		do {
-			// MENÚ INICIAL//
-			int elegido = Menu.menuGeneral();
-
-			if (elegido == 1) {// JUGAR PARTIDA //
-				for (Personaje personaje : listaPersonajes) {
-
-	                // MUESTRA LOS PERSONAJES //
-	                System.out.println("Personajes disponibles:");
-					personaje.imprimirPersonaje();
-					salirMenu = true;
-				}
-		 }else if(elegido==2) {
-			 Menu.instrucciones();		 
-		 }else {
-			  System.out.println("has salido del juego");
-			 salirJuego=true;
-		 }
-        }while(salirMenu==false && salirJuego==false);
-        
-        // MENÚ SELECCIÓN DE PREGUNTAS //
-        Preguntas listaPreguntas = new Preguntas();
-        if(salirMenu==true) {
-        	int preguntaElegida= Menu.preguntasRasgos();
-			switch(preguntaElegida) {
-        	case 1:
-        		ArrayList<String> genero = listaPreguntas.getPreguntasPorCategoria("Genero");
-        		for (String pregunta : genero) {
-					System.out.println(pregunta);
-				}
-        		int eleccionRasgo= sc.nextInt();
-        		break;
-        	case 2:	
-        		ArrayList<String> colorPelo = listaPreguntas.getPreguntasPorCategoria("ColorPelo");
-        		for (String pregunta : colorPelo) {
-					System.out.println(pregunta);
-				}
-        		eleccionRasgo= sc.nextInt();
-        		break;
-        	case 3:	
-        		ArrayList<String> ojos = listaPreguntas.getPreguntasPorCategoria("Ojos");
-        		for (String pregunta : ojos) {
-					System.out.println(pregunta);
-				}
-        		eleccionRasgo= sc.nextInt();
-        		break;
-        	case 4:	
-        		ArrayList<String> tipoPelo = listaPreguntas.getPreguntasPorCategoria("TipoPelo");
-        		for (String pregunta : tipoPelo) {
-					System.out.println(pregunta);
-				}
-        		eleccionRasgo= sc.nextInt();
-        		break;
-        	case 5:	
-        		ArrayList<String> nombres = listaPreguntas.getPreguntasPorCategoria("Nombres");
-        		for (String pregunta : nombres) {
-					System.out.println(pregunta);
-				}
-        		eleccionRasgo= sc.nextInt();
-        		break;
-        	default:
-        		System.out.println("Respuesta incorrecta introduce un numero del 1 al 5");
-        		
-        	}
-        	
-        	// OPCIÓN SI ELIGUE SALIR DEL JUEGO // 
-        }else if(salirJuego==true) {
-        	System.out.println("Has decidido salir del juego ");
-        	System.out.println("Hasta la próxima");
-        	
-        } 
+		
        
 	}
+
+
 
 }
